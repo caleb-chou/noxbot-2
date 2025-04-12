9125bff1971ee10ad27975cbb81245e4297e632f
caleb-chou <haloisawesome412@gmail.com>
2025-04-12 13:51:47 -0500

    fixed register

--- src/register.js
+++ src/register.js
@@ -1,4 +1,4 @@
-import { INVITE_COMMAND } from './commands.js';
+import { INVITE_COMMAND, TEST_COMMAND } from './commands.js';
 import dotenv from 'dotenv';
 import process from 'node:process';
 
@@ -34,7 +34,7 @@ const response = await fetch(url, {
     Authorization: `Bot ${token}`,
   },
   method: 'PUT',
-  body: JSON.stringify([INVITE_COMMAND]),
+  body: JSON.stringify([INVITE_COMMAND, TEST_COMMAND]),
 });
 
 if (response.ok) {
