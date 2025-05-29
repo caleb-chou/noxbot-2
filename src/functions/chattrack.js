e73c94f3443bdccd6c270fc0575e382c0d02d7d6
caleb-chou <haloisawesome412@gmail.com>
2025-05-28 21:02:21 -0500

    message filtering

--- src/functions/chattrack.js
+++ src/functions/chattrack.js
@@ -1,33 +1,39 @@
 const DISCORD_API = 'https://discord.com/api/v10';
+const WORD_REGEX = /\b(\w{3,}|<:\w+:\w+>)\b/g;
 
-export async function getStatsOnUser(interaction, userId, token) {
+
+export async function getStatsOnUser(interaction, userId, userChatData, token) {
+    const messages = await getUserMessages(interaction.channel_id, userId, userChatData?.lastMessageId, token);
+    const filtered_messages = messages.filter(msg => WORD_REGEX.test(msg.content));
 
 }
 
-async function getUserMessages(channelId, userId, token) {
+async function getUserMessages(channelId, userId, userLastMessageId, token) {
     const messages = [];
     let lastMessageId = null;
-  
-    while (true) {
-      const url = new URL(`https://discord.com/api/v10/channels/${channelId}/messages`);
-      url.searchParams.set('limit', 100);
-      if (lastMessageId) {
-        url.searchParams.set('before', lastMessageId);
-      }
-  
-      const res = await fetch(url, {
-        headers: { Authorization: `Bot ${token}` },
-      });
-  
-      if (!res.ok) throw new Error(`Failed to fetch: ${res.statusText}`);
-      const batch = await res.json();
-      if (batch.length === 0) break;
-  
-      const filtered = batch.filter(msg => msg.author.id === userId);
-      messages.push(...filtered);
-  
-      lastMessageId = batch[batch.length - 1].id;
-    }
-  
+
+    do {
+        const url = new URL(`${DISCORD_API}/channels/${channelId}/messages`);
+        url.searchParams.set('limit', 100);
+        if (lastMessageId) {
+            url.searchParams.set('before', lastMessageId);
+        }
+        
+        if (userLastMessageId) {
+            url.searchParams.set('after', userLastMessageId);
+        }
+
+        const res = await fetch(url, {
+            headers: { Authorization: `Bot ${token}` },
+        });
+
+        if (!res.ok) throw new Error(`Failed to fetch: ${res.statusText}`);
+        const batch = await res.json();
+        const filtered = batch.filter(msg => msg.author.id === userId);
+        messages.push(...filtered);
+        lastMessageId = batch[batch.length - 1].id;
+
+    } while (batch.length > 0);
+
     return messages;
-  }
+}
